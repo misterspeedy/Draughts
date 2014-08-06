4b83e1a0724946519af48690f111e6cdeeba695c
Kit Eason <kit.eason@gmail.com>
2014-08-06 16:03:40 +0100

    Initial board tests
    Some tests for the board (and add reference to main project).

---
+++ Draughts.Tests/Board.Tests.fs
@@ -0,0 +1,35 @@
+﻿[<NUnit.Framework.TestFixture>]
+module BoardTests
+
+open System
+open NUnit.Framework
+
+[<Test>]
+let ``The board is 8x8``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Odd numbered squares are unreachable``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Even numbered squares are reachable``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Reachable squares might be unoccupied``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Reachable squares might be occupied by a piece``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
