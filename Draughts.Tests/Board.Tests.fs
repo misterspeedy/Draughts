d5f0a825d092f3f1b7c4227cd67750f3078a6723
Kit Eason <kit.eason@gmail.com>
2014-08-06 21:33:40 +0100

    First passing test
    Board size.  Likely to change again soon!

--- Draughts.Tests/Board.Tests.fs
+++ Draughts.Tests/Board.Tests.fs
@@ -3,11 +3,13 @@ module BoardTests
 
 open System
 open NUnit.Framework
+open Board
 
 [<Test>]
 let ``The board is 8x8``() =
-   let expected = true
-   let actual = false
+   let sut = EmptyBoard()
+   let expected = 8, 8
+   let actual = sut |> Array2D.length1, sut |> Array2D.length2
    Assert.AreEqual(expected, actual)
 
 [<Test>]
@@ -18,6 +20,12 @@ let ``I can access a square in the board by column and row``() =
 
 [<Test>]
 let ``Accessing a square outside the board returns an error``() =
+   let expected = true
+   let actual = false
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``I cannot update a square``() =
    let expected = true
    let actual = false
    Assert.AreEqual(expected, actual)
---
+++ Draughts/Board.fs
@@ -0,0 +1,4 @@
+﻿module Board
+
+let EmptyBoard() = 
+   Array2D.init 8 8 (fun _ _ -> None)
