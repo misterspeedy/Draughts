4b97b37ae555edd8105c3b65a2dafab5df76a94c
Kit Eason <kit.eason@gmail.com>
2014-08-07 18:12:36 +0100

    Add player alternation
    Make Red the first player and support alternation after that.

--- Draughts.Tests/Player.Tests.fs
+++ Draughts.Tests/Player.Tests.fs
@@ -1,2 +1,26 @@
-﻿module PlayerTests
+﻿[<NUnit.Framework.TestFixture>]
+module PlayerTests
 
+open System
+open NUnit.Framework
+open Player
+
+[<Test>]
+let ``Play starts with red``() =
+   let expected = Red
+   let actual = FirstPlayer
+   Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``White plays after red``() =
+   let expected = White
+   let current = Red
+   let actual = current |> NextPlayer 
+   Assert.AreEqual(expected, actual)
+  
+[<Test>]
+let ``Red plays after white``() =
+   let expected = Red
+   let current = White
+   let actual = current |> NextPlayer 
+   Assert.AreEqual(expected, actual)
--- Draughts/Player.fs
+++ Draughts/Player.fs
@@ -1,3 +1,10 @@
 ﻿module Player
 
 type Player = Red | White
+
+let FirstPlayer = Red
+
+let NextPlayer player =
+   match player with
+   | Red -> White
+   | White -> Red
