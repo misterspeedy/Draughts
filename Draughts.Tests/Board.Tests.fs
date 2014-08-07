2f1d62d98541afdb7eae9fbc6f5ff9f5da273803
Kit Eason <kit.eason@gmail.com>
2014-08-07 17:03:48 +0100

    Stop using ImmutableArray2D
    Stop using ImmutableArray2D and make Board a proper immutable type.  Get
    rid of some code duplication in tests.
    
    ImmutableArray2D still needs to be deleted; may need to copy some of its
    tests.

--- Draughts.Tests/Board.Tests.fs
+++ Draughts.Tests/Board.Tests.fs
@@ -7,16 +7,20 @@ open Board
 open Square
 open Piece
 
+let IsOdd c r = 
+   let index = r * 8 + c
+   index % 2 = 1
+
 [<Test>]
 let ``The board is 8x8``() =
-   let sut = EmptyBoard()
+   let sut = Board()
    let expected = 8, 8
    let actual = sut.Width, sut.Height
    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``I can access any square in the board by column and row``() =
-   let sut = EmptyBoard()
+   let sut = Board()
    for col in 0..sut.Width - 1 do
       for row in 0..sut.Height - 1 do
          let expected = true
@@ -33,7 +37,7 @@ let ``I can access any square in the board by column and row``() =
 [<TestCase(8, 0)>]
 [<TestCase(0, 8)>]
 let ``Accessing a square outside the board returns an error``(row, col) =
-   let sut = EmptyBoard()
+   let sut = Board()
    let cant = (fun () -> sut.[col, row] |> ignore)
    Assert.That(cant, Throws.TypeOf<System.IndexOutOfRangeException>())
 
@@ -45,49 +49,53 @@ let ``I cannot update a square``() =
    Assert.AreEqual(expected, actual)
 
 [<Test>]
-let ``Odd numbered squares are unreachable``() =
-   let isOdd c r = 
-      let index = r * 8 + c
-      index % 2 = 0
-   let sut = EmptyBoard()
+let ``Even numbered squares are unreachable``() =
+   let sut = Board()
    let expected = Square.Unreachable
    for col in 0..sut.Width - 1 do
       for row in 0..sut.Height - 1 do
-         if isOdd col row then 
+         if not (IsOdd col row) then 
             let actual = sut.[col, row]
             Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Reachable squares are initially unoccupied``() =
-   let isOdd r c = 
-      let index = r * 8 + c
-      index % 2 = 0
-   let sut = EmptyBoard()
+   let sut = Board()
    let expected = Square.Unoccupied
    for col in 0..sut.Width - 1 do
       for row in 0..sut.Height - 1 do
-         if not (isOdd row col) then 
+         if IsOdd col row then 
             let actual = sut.[col, row]
             Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``I can place a piece in an unoccupied square producing a new board``() =
-   let sut = EmptyBoard()
-   let piece = Occupied(Piece)
-   let sut' = sut.Set(1, 0, piece)
+   let sut = Board()
+   let square = Occupied(Piece)
+   let sut' = sut.Set(1, 0, square)
    Assert.AreNotEqual(sut, sut')
 
 [<Test>]
 let ``When I have placed a piece I can find that piece in the new board``() =
-   let sut = EmptyBoard()
-   let piece = Occupied(Piece)
-   let sut' = sut.Set(1, 0, piece)
-   let expected = Occupied(Piece)
+   let sut = Board()
+   let square = Occupied(Piece)
+   let sut' = sut.Set(1, 0, square)
+   let expected = square
    let actual = sut'.[1, 0]
    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Placing a piece in an occupied square causes an error``() =
+   let sut = Board()
+   let square = Occupied(Piece)
+   let sut' = sut.Set(1, 0, square)
+   let cant = (fun () -> 
+      let sut'' = sut'.Set(1, 0, square)
+      ())
+   Assert.That(cant, Throws.TypeOf<System.ArgumentException>())
+
+[<Test>]
+let ``Placing a piece in an unreachable square causes an error``() =
    let expected = true
    let actual = false
    Assert.AreEqual(expected, actual)
--- Draughts/Board.fs
+++ Draughts/Board.fs
@@ -1,15 +1,30 @@
 ﻿module Board
 
+open System
+open Square
 open ImmutableArray
 
-let EmptyBoard() = 
-   let data = Array2D.init 8 8 (fun c r -> 
-      let isOdd = 
-         let index = r * 8 + c
-         index % 2 = 0   
-      if isOdd then
-         Square.Unreachable
-      else
-         Square.Unoccupied)
-   ImmutableArray2D(data)
-   
+type Board(data) = 
+   new() =
+      let data = Array2D.init 8 8 (fun col row -> 
+         let isOdd c r = 
+            let index = r * 8 + c
+            index % 2 = 1
+         if isOdd col row then
+            Square.Unoccupied
+         else
+            Square.Unreachable)
+      new Board(data)
+   member this.Width = data |> Array2D.length1
+   member this.Height = data |> Array2D.length2
+   member this.Item(col, row) = data.[col, row]
+   member this.Set(col, row, value) =
+      match data.[col, row] with
+      | Unoccupied -> 
+         let data' = Array2D.copy data
+         data'.[col, row] <- value
+         Board(data')
+      | Occupied _ -> raise (ArgumentException("Square is already occupied"))
+      | _ -> raise(ArgumentException("Case not yet supported"))
+
+   // TODO consider copying tests from ImmutableArray
